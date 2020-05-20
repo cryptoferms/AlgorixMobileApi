747bf1587a8486f306833f55c170c5af232a3642
fernando Schilipack <nando_fernendo@hotmail.com>
2020-05-20 12:07:52 -0300

    Add project files.

---
+++ AlgorixMobileApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AlgorixMobileApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ AlgorixMobileApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AlgorixMobileApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
